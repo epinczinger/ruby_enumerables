b239a657c5f506a5f25508d6227ad19dbd4aa3c5
Daniel Sokil <dtechtours@gmail.com>
2020-06-29 18:54:58 -0400

    Implemented my_all?

--- enumerables.rb
+++ enumerables.rb
@@ -10,7 +10,15 @@ module ::Enumerable
     end
   end
 
-  # def my_all?end
+  # Passes each element of the collection to the given block.
+  # The method returns true if the block never returns false or nil.
+  def my_all?(&block)
+    my_each_with_index do |item, _index|
+      false if block.call(item) != true
+    end
+
+    true
+  end
 
   # def my_none?end
 
@@ -18,3 +26,6 @@ module ::Enumerable
 end
 
 [9, 8, 7, 6, 5, 4, 3, 2, 1].my_each_with_index { |item, index| p([index, item]) }
+
+raise("#my_all 0-1 Failed") unless %w[ant bear cat].my_all? { |word| word.length >= 3 } == true
+raise("#my_all 0-1 Failed") unless %w[ant bear cat].all? { |word| word.length >= 4 } == false
