0050c72c97df7ce71b4db9d4b2cf7ec9a377561d
Daniel Sokil <dtechtours@gmail.com>
2020-07-02 13:01:56 -0400

    Testing Comments

--- enumerable_spec.rb
+++ enumerable_spec.rb
@@ -2,8 +2,10 @@ require("./enumerable")
 
 # [9, 8, 7, 6, 5, 4, 3, 2, 1].my_each_with_index { |item, index| p([index, item]) }
 
+# Testing `#my_all`
 raise("#my_all 0-1 Failed") unless %w[ant bear cat].my_all? { |word| word.length >= 3 } == true
 raise("#my_all 0-2 Failed") unless %w[ant bear cat].my_all? { |word| word.length >= 4 } == false
 
+# Testing `#my_none`
 raise("#my_none? 0-1 Failed") unless %w[ant bear cat].my_none? { |word| word.length == 5 } == true
 raise("#my_none? 0-2 Failed") unless %w[ant bear cat].my_none? { |word| word.length >= 4 } == false
