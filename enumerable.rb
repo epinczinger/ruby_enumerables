9c9941371ab0171bffd421e3e13a75f100909bc7
Daniel Sokil <dtechtours@gmail.com>
2020-07-03 15:12:32 -0400

    Added Comments

--- enumerable.rb
+++ enumerable.rb
@@ -3,14 +3,13 @@ module ::Enumerable
   # Esteban: 3, 5, 7, 9, 11
   # Daniel: 4, 6, 8, 10
 
-  
+  # Calls block with the item for each item in enum.
   def my_each
     return to_enum unless block_given?
 
     length.times { |n| yield self[n] }
     self
   end
-
 
   # Loops, Calling The Block With Item And Index
   def my_each_with_index(&block)
@@ -19,7 +18,7 @@ module ::Enumerable
     end
   end
 
-
+  # Returns an array containing all elements of enum for which the given block returns a true value.
   def my_select
     return to_enum unless block_given?
 
@@ -40,6 +39,8 @@ module ::Enumerable
     true
   end
 
+  # Passes each element of the collection to the given block.
+  # The method returns true if the block ever returns a value other than false or nil.
   def my_any?(&block)
     my_each_with_index do |item, _index|
       return true if block.call(item) == true
@@ -58,13 +59,16 @@ module ::Enumerable
     true
   end
 
+  # Returns the number of items in enum through enumeration.
+  # If an argument is given, the number of items in enum that are equal to item are counted.
+  # If a block is given, it counts the number of elements yielding a true value.
   def my_count(item = nil)
     return size if item.nil? && !block_given?
 
     count = 0
     my_each do |x|
       if block_given?
-        count += 1 if yield x
+        count += 1 if yield(x)
       elsif x == item
         count += 1
       end
@@ -82,6 +86,4 @@ module ::Enumerable
 
     transformed
   end
-
-  # my_inject
 end
